25698ab351c013410da1e59460bb124659dfce8f
mihailohranisavljevic <mihailoh@stud.ntnu.no>
2025-01-29 14:28:26 +0100

    BoardTest small fix

--- src/test/java/edu/ntnu/idi/bidata/BoardTest.java
+++ src/test/java/edu/ntnu/idi/bidata/BoardTest.java
@@ -5,30 +5,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
 
+
   @Test
-  void testInitializeBoard() {
+  void initializeBoard() {
     Board board = new Board();
-    assertEquals(4, board.getTile(0).getPosition());
+    assertEquals(0, board.getTile(0).getPosition());
     assertEquals("Start", board.getTile(0).getName());
-    assertEquals(1, board.getTile(1).getPosition());
-    assertEquals("A", board.getTile(1).getName());
-    assertEquals(2, board.getTile(2).getPosition());
-    assertEquals("B", board.getTile(2).getName());
-    assertEquals(3, board.getTile(3).getPosition());
-    assertEquals("C", board.getTile(3).getName());
   }
-
   @Test
-  void testGetTile() {
+  void getTile() {
     Board board = new Board();
-    Tiles tile = board.getTile(1);
+    Tiles tile = board.getTile(0);
     assertNotNull(tile);
-    assertEquals(1, tile.getPosition());
-    assertEquals("A", tile.getName());
+    assertEquals(0, tile.getPosition());
+    assertEquals("Start", tile.getName());
   }
-
   @Test
-  void testGetTileInvalidPosition() {
+  void getTileInvalidPosition() {
     Board board = new Board();
     assertThrows(IllegalArgumentException.class, () -> board.getTile(-1));
     assertThrows(IllegalArgumentException.class, () -> board.getTile(4));
