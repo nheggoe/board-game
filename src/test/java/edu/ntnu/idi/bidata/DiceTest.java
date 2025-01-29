180c50bbcb38aa767c46b6669e928530ec19b0d5
mihailohranisavljevic <mihailoh@stud.ntnu.no>
2025-01-29 11:36:12 +0100

    DiceTest implementation.

---
+++ src/test/java/edu/ntnu/idi/bidata/DiceTest.java
@@ -0,0 +1,17 @@
+package edu.ntnu.idi.bidata;
+
+import static org.junit.jupiter.api.Assertions.*;
+class DiceTest {
+
+  @org.junit.jupiter.api.Test
+  void roll() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void isDouble() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void getTotal() {
+  }
+}
